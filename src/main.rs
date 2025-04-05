83367135463f5980ecbe8fd9581445e4f0c5f67c
raznox <konten@zvaadw.de>
2025-04-05 21:54:41 +0200

    implemented nonactive fish game logic. implement simple selection strategy. moved from four static structs to an array of fixed length for fish.

--- src/main.rs
+++ src/main.rs
@@ -19,36 +19,42 @@ struct Cli {
     n: u16,
 }
 
-//TODO struct GameBoard
+fn dice_roll(rng: &mut ThreadRng) -> &str {
+    let random_index = rng.random_range(0..DIE_SIDES.len());
+    return DIE_SIDES[random_index];
+}
+
 
 struct Piece<'a> {
     position: u16,
     state: &'a str,
 }
 
-fn dice_roll(rng: &mut ThreadRng) -> &str {
-    let random_index = rng.random_range(0..DIE_SIDES.len());
-    return DIE_SIDES[random_index];
-}
-
-fn check_win_condition(
-    boat: &Piece,
-    blue_fish: &Piece,
-    orange_fish: &Piece,
-    pink_fish: &Piece,
-    yellow_fish: &Piece,
-) -> bool {
+fn check_win_condition(boat: &Piece, fish: &[Piece]) -> bool {
     if boat.position == 0 {
         return true;
     }
-    if blue_fish.state != "active"
-        && orange_fish.state != "active"
-        && pink_fish.state != "active"
-        && yellow_fish.state != "active" {
-        return true;
-    }
-    return false;
+    return fish.iter().any(|piece| piece.state == "active") == false;
 }
+
+fn determine_winner<'a>(fish: &[Piece]) -> &'a str {
+    let num_fish_escaped = fish.iter().filter(|&fish| fish.state == "free").count();
+    if num_fish_escaped >= 2 {
+        return WINNERS[0];
+    } else {
+        return WINNERS[1];
+    }
+}
+
+//TODO move this to a pure function.
+fn update_fish_state<'a>(fish: &'a mut Piece<'_>, boat: &Piece<'_>) {
+    if fish.position == 0 {
+        fish.state = "free";
+    } else if boat.position <= fish.position {
+        fish.state = "captured";
+    }
+}
+
 
 fn run_game(cli: &Cli) -> &str {
     /*Game board setup:
@@ -61,111 +67,80 @@ fn run_game(cli: &Cli) -> &str {
         position: cli.fish_tiles + cli.boat_tiles + 2,
         state: "active",
     };
-    //TODO optimize: hashmap color -> Piece
-    let mut yellow_fish = Piece {
-        position: cli.fish_tiles + 1,
-        state: "active",
-    };
-    let mut blue_fish = Piece {
-        position: cli.fish_tiles + 1,
-        state: "active",
-    };
-    let mut pink_fish = Piece {
-        position: cli.fish_tiles + 1,
-        state: "active",
-    };
-    let mut orange_fish = Piece {
-        position: cli.fish_tiles + 1,
-        state: "active",
-    };
+
+    /*
+     Fixed size array, because HashMap would allocate on the heap, and i don't want that.
+     Mapping is:
+        [0] Blue
+        [1] Orange
+        [2] Pink
+        [3] Yellow
+    */
+    let fish_start = cli.fish_tiles + 1;
+    let mut fish = [
+        Piece {
+            position: fish_start,
+            state: "active",
+        },
+        Piece {
+            position: fish_start,
+            state: "active",
+        },
+        Piece {
+            position: fish_start,
+            state: "active",
+        },
+        Piece {
+            position: fish_start,
+            state: "active",
+        },
+    ];
     //loop:
-    while !check_win_condition(&boat, &blue_fish, &orange_fish, &pink_fish, &yellow_fish) {
+    while !check_win_condition(&boat, &fish) {
         // Dice Roll:
         let mut rng = rand::rng();
         let current_dice_roll = dice_roll(&mut rng);
-        match current_dice_roll {
-            "red" => boat.position = boat.position - 1,
-            "green" => boat.position = boat.position - 1,
-            "blue" => blue_fish.position = update_fish_position(&blue_fish), //TODO only if fish is active
-            "orange" => orange_fish.position = update_fish_position(&orange_fish), //TODO only if fish is active
-            "pink" => pink_fish.position = update_fish_position(&pink_fish), //TODO only if fish is active
-            "yellow" => yellow_fish.position = update_fish_position(&yellow_fish), //TODO only if fish is active
-            _ => panic!("this is a terrible mistake"),
-        };
-        update_fish_state(&mut blue_fish, &boat);
-        update_fish_state(&mut orange_fish, &boat);
-        update_fish_state(&mut pink_fish, &boat);
-        update_fish_state(&mut yellow_fish, &boat);
-        //print_game_board(&boat, &blue_fish, &orange_fish, &pink_fish, &yellow_fish);
+        if current_dice_roll == "red" || current_dice_roll == "green" {
+            boat.position -= 1
+        } else {
+            let fish_index: usize = match current_dice_roll {
+                "blue" => 0,
+                "orange" => 1,
+                "pink" => 2,
+                "yellow" => 3,
+                _ => panic!("this is a terrible mistake"),
+            };
+            //TODO filter + map?
+            let selected_fish_state = fish[fish_index].state;
+            if selected_fish_state == "active" {
+                fish[fish_index].position -= 1;
+            } else if selected_fish_state == "captured" {
+                boat.position -= 1
+            } else if selected_fish_state == "free" {
+                // This is just one of many strategies we could use:
+                // Move the next active fish we found:
+                for current_fish in &mut fish {
+                    if current_fish.state == "active" {
+                        current_fish.position -= 1;
+                        break;
+                    }
+                }
+                // TODO other strategies
+                // Move the fish that's nearest the sea
+                // Move the fish that's farthest from the sea (but still active)
+            } else {
+                panic!("this is a terrible mistake");
+            }
+        }
+        for current_fish in &mut fish {
+            update_fish_state(current_fish, &boat);
+        }
+        //print_game_board(&boat, &fish[0], &fish[1], &fish[2], &fish[3]);
     }
-    let winner = determine_winner(&blue_fish, &orange_fish, &pink_fish, &yellow_fish);
+    let winner = determine_winner(&fish);
     //println!("{}", winner);
     return winner;
 }
-
-//TODO move this to a pure function.
-fn update_fish_state<'a>(fish: &'a mut Piece<'_>, boat: &Piece<'_>) {
-    if fish.position == 0 {
-        fish.state = "free";
-    } else if boat.position <= fish.position {
-        fish.state = "captured";
-    }
-}
-
-fn update_fish_position(fish: &Piece<'_>) -> u16 {
-    if fish.state != "active" {
-        //TODO this is not the original logic: in the original,
-        // if the fish is already "free": move another fish
-        // if the fish is already "captured": move the boat.
-        return fish.position;
-    } else {
-        return fish.position - 1;
-    }
-}
-
-fn determine_winner<'a>(
-    blue_fish: &Piece,
-    orange_fish: &Piece,
-    pink_fish: &Piece,
-    yellow_fish: &Piece,
-) -> &'a str {
-    let mut num_fish_escaped: u8 = 0;
-    if blue_fish.state == "free" {
-        num_fish_escaped += 1;
-    }
-    if orange_fish.state == "free" {
-        num_fish_escaped += 1;
-    }
-    if pink_fish.state == "free" {
-        num_fish_escaped += 1;
-    }
-    if yellow_fish.state == "free" {
-        num_fish_escaped += 1;
-    }
-
-    if num_fish_escaped >= 2 {
-        return WINNERS[0];
-    } else {
-        return WINNERS[1];
-    }
-}
-
-/*fn print_game_board(
-    boat: &Piece,
-    blue_fish: &Piece,
-    orange_fish: &Piece,
-    pink_fish: &Piece,
-    yellow_fish: &Piece,
-) {
-    println!(
-        "Current Game State: boat{},b{},o{},p{},y{}",
-        boat.position,
-        blue_fish.position,
-        orange_fish.position,
-        pink_fish.position,
-        yellow_fish.position
-    );
-}*/
 
 fn main() {
     let cli = Cli::parse();
